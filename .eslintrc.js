bfee743db8feaad41bd4061a68d66ba61ca26891
Dream-kid-342 <mumoenock342@gmail.com>
2026-05-26 17:24:31 +0300

    fixed CI/CD pipeline

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off'
+  },
+  ignorePatterns: ['dist', 'node_modules', 'build', 'apps/mobile'],
+};
